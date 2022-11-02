fee6c41c92d8e435d8d86604e7d2932705549ae0
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-11-02 01:36:42 +0100

    Add relativeUrl to library song model and view

--- app/src/main/java/com/bodzify/model/LibrarySong.kt
+++ app/src/main/java/com/bodzify/model/LibrarySong.kt
@@ -6,6 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class LibrarySong (
     val uuid: String,
+    val relativeUrl: String,
     val filename: String,
     val fileExtension: String,
     val title: String?,
