042a740c4ae98fda3402dfbea9a407d886bf7f67
Andreas-Garcia <garcia.andreas.1991@gmail.com>
2022-11-10 15:14:20 +0100

    Logout when refreshtoken expired

--- app/src/main/java/com/bodzify/datasource/repository/BaseRepository.kt
+++ app/src/main/java/com/bodzify/datasource/repository/BaseRepository.kt
@@ -1,11 +1,18 @@
 package com.bodzify.datasource.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.bodzify.datasource.network.api.BaseApi
 import com.bodzify.datasource.network.api.SafeApiCall
 
 open class BaseRepository (private val api: BaseApi) : SafeApiCall {
 
+    private val logoutPerformedMutableLiveData = MutableLiveData<Boolean>()
+    val logoutPerformedLiveData: LiveData<Boolean>
+        get() = logoutPerformedMutableLiveData
+
     suspend fun logout() = safeApiCall {
         api.logout()
+        logoutPerformedMutableLiveData.postValue(true)
     }
 }
