1883c813949c4000ed3a71796b6b24f50122393e
Andréas <garcia.andreas.1991@gmail.com>
2023-01-04 10:38:24 +0100

    Clean code

--- app/src/main/java/com/bodzify/ui/fragment/FullScreenPlayerFragment.kt
+++ app/src/main/java/com/bodzify/ui/fragment/FullScreenPlayerFragment.kt
@@ -1,25 +1,13 @@
 package com.bodzify.ui.fragment
 
-import android.content.Intent
-import android.media.AudioAttributes
-import android.media.MediaPlayer
 import android.os.Bundle
-import android.provider.AlarmClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.core.content.ContextCompat
 import com.bodzify.R
-import com.bodzify.datasource.network.api.RemoteDataSource
 import com.bodzify.model.LibraryTrack
-import com.bodzify.ui.activity.FullScreenPlayerActivity
-import com.bodzify.ui.activity.TrackEditionActivity
-import com.bodzify.viewmodel.PlayingTrackViewModel
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 class FullScreenPlayerFragment (
     playingTrackViewModel: PlayingTrackViewModel,
